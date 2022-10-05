832edaa414db7972c52f3bf69405d275ca1bef58
wooyoungkwak <zilet123456.gmail.com>
2022-10-06 08:41:26 +0900

    API 수정 작업중 ...

--- src/main/java/com/teraenergy/illegalparking/dialect/MyMariaDB103Dialect.java
+++ src/main/java/com/teraenergy/illegalparking/dialect/MyMariaDB103Dialect.java
@@ -13,14 +13,16 @@ import org.hibernate.type.StandardBasicTypes;
 public class MyMariaDB103Dialect extends MariaDB103Dialect {
 
     /**
+     * Mariadb 의 GEOMETRY 함수를 추가한 Dialect
+     *
      * 적용 방법
      *  - application.yml 파일 수정 -
      *  spring:
      *      ...
      *      jpa:
      *        database-platform: org.hibernate.dialect.MariaDB103Dialect  <<< 삭제
-     *        database-platform: com.teraenergy.illegalparking.dialect.MyMariaDB103Dialect
-     * */
+     *        database-platform: com.teraenergy.illegalparking.dialect.MyMariaDB103Dialect <<< 추가
+     */
     public MyMariaDB103Dialect() {
         super();
         this.registerFunction( "ASTEXT", new StandardSQLFunction( "ASTEXT", StandardBasicTypes.CHARACTER) );
